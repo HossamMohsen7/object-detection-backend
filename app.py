ae054331e0fa9e0d49911de6ffaf92df63b27fb3
Hossam Mohsen <6911267+HossamMohsen7@users.noreply.github.com>
2024-05-19 00:05:37 +0300

    Model

--- app.py
+++ app.py
@@ -1,5 +1,16 @@
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
+from PIL import Image
+from transformers import (
+    BlipProcessor,
+    BlipForConditionalGeneration,
+    MarianMTModel,
+    MarianTokenizer,
+)
+from base64 import b64decode
+from io import BytesIO
+import time
+
 
 app = FastAPI()
 
@@ -11,10 +22,50 @@ app.add_middleware(
     allow_headers=["*"],
 )
 
+processor = BlipProcessor.from_pretrained("Salesforce/blip-image-captioning-large")
+model = BlipForConditionalGeneration.from_pretrained(
+    "Salesforce/blip-image-captioning-large"
+)
+# Initialize the MarianMT model and tokenizer for translation
+translation_model_name = "Helsinki-NLP/opus-mt-en-ar"
+translation_model = MarianMTModel.from_pretrained(translation_model_name)
+translation_tokenizer = MarianTokenizer.from_pretrained(translation_model_name)
+
+
+def generate_caption(img):
+    raw_image = img.convert("RGB")
+
+    inputs = processor(raw_image, return_tensors="pt", max_new_tokens=100)
+
+    start_time = time.time()
+    out = model.generate(**inputs)
+    generation_time = time.time() - start_time
+
+    caption = processor.decode(out[0], skip_special_tokens=True)
+
+    # Translate the caption into Arabic
+    translated_inputs = translation_tokenizer(caption, return_tensors="pt")
+    translated_out = translation_model.generate(**translated_inputs)
+    translated_caption = translation_tokenizer.decode(
+        translated_out[0], skip_special_tokens=True
+    )
+
+    return translated_caption
+
 
 @app.get("/")
 async def main():
     return {"message": "Hello World"}
+
+
+# json data that has "image" as base64 string
+@app.post("/predict")
+async def predict(data: dict):
+    image = data["image"]
+    # convert the base64 string to an image
+    image = Image.open(BytesIO(b64decode(image)))
+    caption = generate_caption(image)
+    return {"message": caption}
 
 
 if __name__ == "__main__":
